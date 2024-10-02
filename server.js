0e6d4e7cbf401ddd28f6862315f671ff3d831cad
1Light <redclover1.618@gmail.com>
2024-10-02 20:05:02 +0300

    upload

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-import express from 'express';
-import controllerRouting from './routes/index';
-
-/**
- * This project is a summary of back-end concepts:
- * authentication, NodeJS, MongoDB, Redis,
- * pagination and background processing.
- *
- * The objective was to build a simple platform to upload and view files:
- *
- * User authentication via a token
- * List all files
- * Upload a new file
- * Change permission of a file
- * View a file
- * Generate thumbnails for images
- */
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(express.json());
-
-controllerRouting(app);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
