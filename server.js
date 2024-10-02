70a19f43497d9d09b6dcd8fbcc90a09ee5baacde
1Light <redclover1.618@gmail.com>
2024-10-02 19:59:23 +0300

    upload

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import express from 'express';
-import Routes from './routes/index';
+import controllerRouting from './routes/index';
+
+/**
+ * This project is a summary of back-end concepts:
+ * authentication, NodeJS, MongoDB, Redis,
+ * pagination and background processing.
+ *
+ * The objective was to build a simple platform to upload and view files:
+ *
+ * User authentication via a token
+ * List all files
+ * Upload a new file
+ * Change permission of a file
+ * View a file
+ * Generate thumbnails for images
+ */
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-Routes(app);
+
+controllerRouting(app);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- utils/db.js
+++ utils/db.js
@@ -5,31 +5,51 @@ const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
+/**
+ * Class for performing operations with Mongo service
+ */
 class DBClient {
   constructor() {
-    MongoClient.connect(url, (error, client) => {
-      if (error) {
-        console.log(error.message);
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
         this.db = false;
-        return;
       }
-      this.db = client.db(DB_DATABASE);
-      this.users = this.db.collection('users');
-      this.files = this.db.collection('files');
     });
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
     return !!this.db;
   }
 
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
   async nbUsers() {
-    return this.users.countDocuments();
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
   async nbFiles() {
-    return this.files.countDocuments();
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
+
 const dbClient = new DBClient();
+
 export default dbClient;
